1c3b7e471dff8d80e30a584aabf1fbf79a1b7197
Rubin <rubinelezi0@gmail.com>
2022-03-23 17:00:14 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Motive</title>
+        <meta name="description" content="Motive" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-3xl font-bold underline">
+      Hello wssasdsadwefsawdewadewafeaorssld!
+      </h1>
+    </div>
+  )
+}
