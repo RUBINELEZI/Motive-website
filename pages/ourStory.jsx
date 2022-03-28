44a3a73395e641a30fe32d86e094f46287ab484e
Rubin <rubinelezi0@gmail.com>
2022-03-28 16:30:57 +0200

    Build Fix

--- pages/ourStory.jsx
+++ pages/ourStory.jsx
@@ -11,7 +11,7 @@ export default function OurStory() {
         </h1>
         <p className="leading-normal text-base  mb-8 text-center md:text-left z-10">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry standard dummy text ever
           since the 1500s,
         </p>
 
@@ -45,7 +45,7 @@ export default function OurStory() {
 
         <p className="leading-normal text-base mb-8 text-center md:text-left z-10">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
