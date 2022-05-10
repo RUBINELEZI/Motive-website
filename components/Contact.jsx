2fc906b48cec2be31b76a40c13659729936f9c4c
cayd33 <devcaydee@gmail.com>
2022-05-10 17:30:05 +0200

    form test

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -22,16 +22,61 @@ export default function Contact({ data }) {
               </p>
             </div>
 
-            <form name="contact" netlify>
-              <p>
-                <label>Name <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Email <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+            <form className="w-full max-w-lg bg-gray-700 bg-opacity-40 p-10 rounded-2xl" name="contact"
+                  data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label className="block uppercase tracking-wide text-grasy-400 text-xs font-bold mb-2"
+                         htmlFor="grid-first-name">
+                    First Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-600 text-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-500"
+                    id="grid-first-name" type="text" placeholder="" name="name" />
+                </div>
+                <div className="w-full md:w-1/2 px-3">
+                  <label className="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
+                         htmlFor="grid-last-name">
+                    Last Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-600 text-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-gray-500"
+                    id="grid-last-name" type="text" placeholder="" name="lastName" />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
+                         htmlFor="grid-password">
+                    E-mail
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-600 text-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-500 focus:border-gray-500"
+                    id="email" type="email" name="email" />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
+                         htmlFor="grid-password">
+                    Message
+                  </label>
+                  <textarea
+                    className="no-resize appearance-none block w-full bg-gray-600 text-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-500 focus:border-gray-500 h-48 resize-none"
+                    id="message" name="message" />
+                </div>
+              </div>
+              <div className="md:flex md:items-center">
+                <div className="md:w-1/3">
+                  <button
+                    className="shadow bg-purple-400 hover:bg-purple-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    type="submit">
+                    Send
+                  </button>
+                </div>
+                <div className="md:w-2/3" />
+              </div>
             </form>
           </div>
         </div>
